c0eb173ea03c346df79a6eff366ad767cfd9f106
Prince Ab <ajanifiles90@gmail.com>
2026-08-25 23:40:40 +0800

    main resource script spinup0

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    docker = {
+        source = "kreuzwerker/docker"
+        version = "4.5.0"
+    }
+  }
+}
+
+# Configure the docker provider
+provider "docker" {}
---
+++ network.tf
@@ -0,0 +1,3 @@
+resource "docker_network" "openmetadata_net" {
+  name = "openmetadata-net"
+}
